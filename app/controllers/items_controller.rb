577cc73adb021f04cef0705060b22a25d662a2e6
Philippe Aka <philippeaka@outlook.com>
2015-04-15 23:33:29 -0400

    fix routes,controller,views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,24 @@
 class ItemsController < ApplicationController
+
+
   def create
-    @item = current_user.items.create(item_params)
+    @item = Item.new
+    @item = current_user.items.build(item_params)
 
     if @item.save
-       redirect_to user_path(current_user), notice: "Task was saved successfully."
+       redirect_to :back, notice: "Task was saved successfully."
      else
        flash[:error] = "Error creating task. Please try again."
-       redirect_to user_path(current_user), notice: 'name field empty'
+       redirect_to :back, notice: 'name field empty'
      end
   end
 
   def destroy
-    @user = User.find(params[:user_id])
-    @item = @user.items.find(params[:id])
+    @item = Item.find(params[:id])
     
     if @item.destroy
       flash[:notice] = "\"#{@item.name}\" was deleted successfully."
-      redirect_to user_path(current_user)
+      redirect_to :back
     else
       flash[:error] = "There was an error deleting the item."
       render :show
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,13 @@ Rails.application.routes.draw do
 
  
 
-  get 'items/create'
 
   devise_for :users
+  resources :items
+  
   get 'welcome/index'
 
-  resources :users, only: [:show] do 
-    resources :items, only: [:create, :destroy]
-  end
+
   
 
   root to:'users#show', as: :authenticated_root
