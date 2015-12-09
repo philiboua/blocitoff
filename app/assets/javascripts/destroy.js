70f4d5cdc6a1b1e22da4388bf25d3a626cbad6e2
Philippe Aka <philippeaka@outlook.com>
2015-12-09 08:21:53 -0500

    removed javascript file and styled layout

--- app/assets/javascripts/destroy.js
+++
@@ -1,6 +0,0 @@
-
-<% if @application.destroyed? %>
-  $('#application_' +<%= @application.id %>).hide();
-<% else %>
-  $('#application_' +<%= @application.id %>).prepend("<div class='alert alert-danger'><%= flash[:error] %></div>");
-<% end %>
